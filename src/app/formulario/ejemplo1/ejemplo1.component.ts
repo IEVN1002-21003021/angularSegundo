4a3c1bca2266eb8f7674d7870558495484d215a3
America Fernanda Ramirez Rodriguez <fernmirz20@gmail.com>
2024-10-12 10:28:13 -0600

    rutas

--- src/app/formulario/ejemplo1/ejemplo1.component.ts
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -16,6 +16,14 @@ interface Usuarios{
 })
 export class Ejemplo1Component implements OnInit {
   formGroup!:FormGroup;
+
+  materia='pwa'
+  tem:string=''
+  alumnos:Usuarios={
+    nombre: '',
+    edad:0,
+    email:''
+  }
   
   constructor(private fb:FormBuilder){}
 
@@ -31,7 +39,26 @@ export class Ejemplo1Component implements OnInit {
     })
   }
   onSubmit():void{
-    console.log(this.formGroup.value)
+
+    const{nombre,edad,email}=this.formGroup.value;
+    this.alumnos.nombre=nombre;
+    this.alumnos.edad=edad;
+    this.alumnos.email=email;
+    let alumnoJSON=JSON.stringify(this.alumnos);
+
+    console.log(this.formGroup.value);
+
+    localStorage.setItem('materia',this.materia);
+    localStorage.setItem('alumno',alumnoJSON);
+  }
+
+  subImprimir():void{
+    this.tem=localStorage.getItem('materia')!
+    
+    const alumnoGuardado=localStorage.getItem('alumno')
+    if(alumnoGuardado){
+      const alumno:Usuarios=JSON.parse(alumnoGuardado)
+    }
   }
 
 }
